190313824dbcb791fd05781d881e512baa417913
Snixies <79220714+Snixies@users.noreply.github.com>
2021-05-19 11:23:15 +0200

    startup springboot application

---
+++ src/main/java/Snixies/beercatalogservice/BeerCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package Snixies.beercatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCatalogServiceApplication.class, args);
+	}
+
+}
