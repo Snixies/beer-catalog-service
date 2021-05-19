835294eb5bff0ddd37bcc35a8b0d7bd79399ab3d
Snixies <79220714+Snixies@users.noreply.github.com>
2021-05-19 15:27:37 +0200

    added connection to beer info svc

---
+++ src/main/java/Snixies/beercatalogservice/models/Beer.java
@@ -0,0 +1,42 @@
+package Snixies.beercatalogservice.models;
+
+public class Beer {
+
+    private int beerId;
+    private String name;
+    private String description;
+
+    public Beer(){
+
+    }
+
+    public Beer(int beerId, String name, String description) {
+        this.beerId = beerId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getBeerId() {
+        return beerId;
+    }
+
+    public void setBeerId(int beerId) {
+        this.beerId = beerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/Snixies/beercatalogservice/resources/BeerCatalogResource.java
+++ src/main/java/Snixies/beercatalogservice/resources/BeerCatalogResource.java
@@ -1,12 +1,17 @@
 package Snixies.beercatalogservice.resources;
 
+import Snixies.beercatalogservice.models.Beer;
 import Snixies.beercatalogservice.models.CatalogItem;
+import Snixies.beercatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +20,24 @@ public class BeerCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalolog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(
-                new CatalogItem("Hertog Jan Weizener", "Weizen bier van brouwerij Hertog Jan", 4)
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        List<Rating> ratings = Arrays.asList(
+              new Rating(1,4),
+              new Rating(2,5)
         );
+
+        return ratings.stream().map(rating -> {
+            Beer beer = restTemplate.getForObject("http://localhost:8082/beers/" + rating.getBeerId(), Beer.class);
+            return new CatalogItem(beer.getName(), beer.getDescription(), rating.getRating());
+
+        } )
+        .collect(Collectors.toList());
+
+        //foreach beerId, call beer info svc and get details
+
+        //put them all together
+
     }
 }
